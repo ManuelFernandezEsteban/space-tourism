f00771a68ff41887ac555f7c9349ab70fea80d32
Manuel1977 <Manuel_fe1977@hotmail.com>
2022-07-30 13:13:15 +0100

    page crew finalizada

---
+++ dots.js
@@ -0,0 +1,62 @@
+
+
+
+
+const dotList = document.querySelector('.dot-indicators');
+
+const dots = dotList.querySelectorAll('button');
+
+let dotFocus=0;
+
+dotList.addEventListener('keydown',(e)=>{
+    //console.log(e.keyCode)
+    const keyDownLeft = 37;
+    const keyDownRight = 39;
+
+    if ((e.keyDode===keyDownLeft)||(e.keyCode===keyDownRight)){
+
+        dots[dotFocus].setAttribute('tabindex',-1)
+    }
+    if (e.keyCode===keyDownRight){
+        dotFocus++;
+        if (dotFocus>=dots.length){
+            dotFocus=0;
+        }
+    }
+    if (e.keyCode===keyDownLeft){
+        dotFocus--;
+        if (dotFocus<0){
+            dotFocus=dots.length-1;
+        }
+    }
+
+    dots[dotFocus].setAttribute('tabindex',0);
+    dots[dotFocus].focus();
+})
+
+dots.forEach((dot)=>{
+    dot.addEventListener('click',(e)=>{
+        const targetButton = e.target;
+        const targetPanel = targetButton.getAttribute('aria-controls');
+        const mainPanel = document.querySelector('#main');
+        const targetImage = targetButton.getAttribute('data-image');
+        const tabContainer = targetButton.parentNode;
+        tabContainer.querySelector('[aria-selected=true]').setAttribute('aria-selected',false);
+
+        targetButton.setAttribute('aria-selected',true)
+
+        mainPanel.querySelectorAll('[role="tabpanel"]').forEach((article)=>{
+            article.setAttribute('hidden',true);
+        })
+
+        mainPanel.querySelector([`#${targetPanel}`]).removeAttribute('hidden');
+
+        mainPanel.querySelectorAll('img').forEach((image)=>{
+            image.classList.add('sr-only');
+        })
+        mainPanel.querySelector([`#${targetImage}`]).classList.remove('sr-only');
+
+        //console.log(mainPanel);
+    })
+})
+
